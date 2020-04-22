71c73dc4a01d5749a47b297459bae5f89210014f
jiws829 <43372042+jiws829@users.noreply.github.com>
2020-04-22 14:49:40 +0900

    Add files via upload

---
+++ BOJ 13458.cpp
@@ -0,0 +1,36 @@
+﻿/*
+<13458 - 시험 감독>
+먼저 응시자수 에서 총감독관 수를  빼주고
+이후 부감독관수로 나눠서 감독관 수를 세면 됨. 딱 나눠떨어지지 않는 경우도 고려해야함
+
+감독관 수를 세는 변수는 int 범위를 초과할 수 있으므로 long long으로 선언함
+*/
+#include<stdio.h>
+
+int n;
+int p[1000000];
+int b, c;
+long long count = 0;
+
+int main() {
+	int i;
+	int num;
+
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) scanf("%d", &p[i]);
+	scanf("%d %d", &b, &c);
+
+	for (i = 0; i < n; i++) {
+		p[i] -= b;
+		count++;
+
+		if (p[i] <= 0) continue;
+		else {
+			if (p[i] % c == 0) { num = p[i] / c; }
+			else { num = p[i] / c; num++; }
+
+			count += num;
+		}
+	}
+	printf("%lld\n", count);
+}
---
+++ BOJ 14499.cpp
@@ -0,0 +1,68 @@
+﻿/*
+<14499 - 주사위 굴리기>
+주사위를 입력으로 주어진 방향대로 굴리면서 각 면을 잘 이동시켜주면 된다
+이동 후 바닥면과 지도를 비교하며 값을 바꿔주면 됨
+*/
+#include<stdio.h>
+
+int n, m, x, y, k;
+int p[21][21];  //지도
+int t[1001];   //이동하는 명령  1:동  2:서  3:북  4:남
+int dice[7];  //주사위 1:윗  2:뒷  3:오른  4:왼  5:앞  6:아래
+
+int main() {
+	int i, j;
+	int tmp[7];
+
+	//입력
+	scanf("%d %d %d %d %d", &n, &m, &x, &y, &k);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) scanf("%d", &p[i][j]);
+	}
+	for (i = 1; i <= k; i++) scanf("%d", &t[i]);
+
+	for (int kk = 1; kk <= k; kk++) {
+		for (i = 1; i <= 6; i++) tmp[i] = dice[i];
+
+		if (t[kk] == 1) {  //동
+			if (y + 1 > m - 1) continue;
+			y++;
+			tmp[3] = dice[1];
+			tmp[6] = dice[3];
+			tmp[4] = dice[6];
+			tmp[1] = dice[4];
+		}
+		else if (t[kk] == 2) {  //서
+			if (y - 1 < 0) continue;
+			y--;
+			tmp[4] = dice[1];
+			tmp[6] = dice[4];
+			tmp[3] = dice[6];
+			tmp[1] = dice[3];
+		}
+		else if (t[kk] == 3) {  //북
+			if (x - 1 < 0) continue;
+			x--;
+			tmp[2] = dice[1];
+			tmp[6] = dice[2];
+			tmp[5] = dice[6];
+			tmp[1] = dice[5];
+		}
+		else {  //남
+			if (x + 1 > n - 1) continue;
+			x++;
+			tmp[5] = dice[1];
+			tmp[6] = dice[5];
+			tmp[2] = dice[6];
+			tmp[1] = dice[2];
+		}
+
+
+		if (p[x][y] == 0) p[x][y] = tmp[6];
+		else { tmp[6] = p[x][y]; p[x][y] = 0; }
+		printf("%d\n", tmp[1]);
+
+		for (i = 1; i <= 6; i++) dice[i] = tmp[i];
+	}
+
+}
