64b5ea9c4614a54e56e101c469c09330b39717c0
jiws829 <43372042+jiws829@users.noreply.github.com>
2020-04-23 15:33:02 +0900

    Add files via upload

---
+++ BOJ 14501.cpp
@@ -0,0 +1,35 @@
+﻿/*
+<14501 - 퇴사>
+DP를 사용해서 품
+맨 뒤에서 부터 for문을 돌면서
+그날 상담을 했을때(ans[i+t[i]]+p[i])와
+그날 상담을 안했을때(ans[i+1])의 비용을 비교한 경우 더 큰 경우를 넣음
+그럼 ans[1]에 상담 최대값이 들어간다
+
+단 ans크기는 17로 했는데 n=15일때 i가 15에서 ans[i+1]을 참조하기 때문에 ans[16]을 참조할수 있음
+
+*/
+#include<stdio.h>
+
+int p[16], t[16];
+int n;
+int ans[17];
+
+int main() {
+	int i;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) scanf("%d %d", &t[i], &p[i]);
+
+	for (i = n; i >= 1; i--) {
+		if (i + t[i] - 1 > n) {
+			ans[i] = ans[i + 1];
+		}
+		else {
+			if (ans[i + 1] > ans[i + t[i]] + p[i]) ans[i] = ans[i + 1];
+			else ans[i] = ans[i + t[i]] + p[i];
+		}
+	}
+
+	printf("%d\n", ans[1]);
+}
---
+++ BOJ 14502.cpp
@@ -0,0 +1,84 @@
+﻿/*
+<14502- 연구소>
+dfs로 벽 3개를 생성할 수 있는 모든 경우의 수를 만든다
+
+이후 벽 3개가 생성될때마다 바이러스를 모두 flood_fill로 퍼뜨린다
+모든 바이러스가 다 퍼지고 안전영역의 개수를 세어 최대값을 갱신해준다.
+*/
+#include<stdio.h>
+
+int n, m;
+int p[9][9];
+int t[9][9];
+int maxx = -999;
+
+void flood(int x, int y) {  //dfs로 인근지역 바이러스로 변환
+	int dx[4] = { -1,0,0,1 }, dy[4] = { 0,-1,1,0 };
+	int i;
+
+	for (i = 0; i < 4; i++) {
+		if (x + dx[i] > n || x + dx[i] < 1 || y + dy[i] > m || y + dy[i] < 1) continue;
+
+		if(t[x + dx[i]][y + dy[i]] == 0 || t[x + dx[i]][y + dy[i]] == 2) {
+			t[x + dx[i]][y + dy[i]] = -1;
+			flood(x + dx[i], y + dy[i]);
+		}
+	}
+}
+
+void virus() {
+	int i, j;
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= m; j++) t[i][j] = p[i][j];
+	}
+	
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= m; j++) {
+			if (t[i][j] == 2) {  //바이러스가 발견되면 그 점부터 flood fill로 인근지역 바이러스로 바꿔줌
+				t[i][j] = -1;
+				flood(i, j);
+			}
+		}
+	}
+	int cnt = 0;
+	for (i = 1; i <= n; i++) {  //안전영역 개수
+		for (j = 1; j <= m; j++) {
+			if (t[i][j] == 0) cnt++;
+		}
+	}
+	if (maxx < cnt) {  //안전영역 최대개수 갱신
+		maxx = cnt;
+	}
+}
+
+void dfs(int v) {
+	int i, j;
+
+	if (v == 3) {  //벽 3개가 다 생성된 경우
+		virus();
+		return;
+	}
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= m; j++) {
+			if (p[i][j] == 0) {
+				p[i][j] = 1;
+				dfs(v + 1);
+				p[i][j] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	int i, j;
+
+	scanf("%d %d", &n, &m);
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= m; j++) scanf("%d", &p[i][j]);
+	}
+
+	dfs(0);  //dfs로 벽 생성
+
+	printf("%d\n", maxx);
+}
