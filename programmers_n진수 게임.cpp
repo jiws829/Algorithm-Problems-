91535dc3fff8f1e24586cb311aae47fa5c20b511
jiws829 <43372042+jiws829@users.noreply.github.com>
2020-05-11 14:11:02 +0900

    Add files via upload

---
+++ programmers_n진수 게임.cpp
@@ -0,0 +1,48 @@
+﻿#include <string>
+#include <vector>
+#include<iostream>
+
+using namespace std;
+
+char c[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+
+    int num = -1;
+    int count = 0;
+    int turn = 1;
+
+    while (count < t) {
+        num++;
+        int tmp = num;
+        int q[100001], cnt = 0;
+        
+        do {
+            q[cnt++] = tmp % n;
+            tmp /= n;
+        } while (tmp > 0);
+
+        for (int i = cnt - 1; i >= 0; i--) {
+
+            if (turn == p) {
+                answer += c[q[i]];
+                count++;
+
+                if (count >= t) break;
+            }
+            turn++;
+            if (turn > m) turn = 1;
+        }
+        if (count >= t) break;
+    }
+
+    
+    return answer;
+}
+
+int main() {
+    string answer = solution(16, 16, 2, 1);
+    //string answer = solution(16, 16, 2, 2);
+    cout << answer << endl;
+}
---
+++ programmers_추석 트래픽.cpp
@@ -0,0 +1,83 @@
+﻿#include <string>
+#include <vector>
+#include<iostream>
+
+#include<algorithm>
+#include<string.h>
+using namespace std;
+
+int check[2001];
+
+class times {
+public:
+    int index;
+    double a, b;
+
+    times(int index, double a, double b) {
+        this->index = index;
+        this->a = a;
+        this->b = b;
+    }
+};
+
+int compare(times a, times b) {
+    if (a.a < b.a) return 1;
+    return 0;
+}
+
+int solution(vector<string> lines) {
+    vector<times> time;
+    int answer = -999;
+    int cnt, cn;
+    int i, j;
+    int h, m;
+    double s, dur;
+
+    for (i = 0; i < lines.size(); i++) {
+        h = (lines[i][11] - '0') * 10 + lines[i][12] - '0';
+        m = (lines[i][14] - '0') * 10 + lines[i][15] - '0';
+        s = (double)(lines[i][17] - '0') * 10 + lines[i][18] - '0' + (lines[i][20] - '0') * 0.1 + (lines[i][21] - '0') * 0.01 + (lines[i][22] - '0') * 0.001;
+
+        s += (double)h * 3600 + m * 60;
+
+        string str = lines[i].substr(24);
+        double tmp = 1.0;
+        dur = 0;
+        for (j = 0; j < str.size() - 1; j++) {
+            if (str[j] == '.') continue;
+            dur += (double)tmp * (str[j] - '0');
+            tmp *= 0.1;
+        }
+        
+        tmp = s - dur;
+        tmp += 0.001;
+
+        if (tmp < 0) tmp = 0;
+        time.push_back(times(i, tmp, s));
+    }
+    sort(time.begin(), time.end(), compare);
+    
+    for (i = 0; i < time.size(); i++) {
+        memset(check, 0, sizeof(check));
+        cnt = cn = 1;
+        for (j = 0; j < i; j++) {
+            if (time[j].a <= time[i].a && time[i].a <= time[j].b) cnt++;
+            else if (time[i].a - time[j].b < 1.0) cnt++;
+        }
+
+        for (j = i + 1; j < time.size(); j++) {
+            if (time[i].b <= time[j].a && time[j].a - time[i].b < 1.0) cn++;
+            else if (time[j].a <= time[i].b && time[i].b <= time[j].b) cn++;
+        }
+        if (answer < cnt) answer = cnt;
+        if (answer < cn) answer = cn;
+    }
+
+    return answer;
+}
+
+int main() {
+    int answer = solution({ "2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s", "2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s" });
+
+    cout << answer << endl;
+}
