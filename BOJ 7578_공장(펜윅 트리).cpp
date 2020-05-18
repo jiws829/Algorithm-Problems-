f54d22ebe2ce66cb96f5001951a0fb9fc6bd9671
jiws829 <43372042+jiws829@users.noreply.github.com>
2020-05-18 14:31:52 +0900

    Add files via upload

---
+++ BOJ 7578_공장(펜윅 트리).cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int n;
+int input[1000001];
+int p[500001];
+long long tree[500001];
+
+void update(int num) {
+	while (num <= n) {
+		tree[num]++;
+		num += (num & -num);
+	}
+}
+
+long long sum(int num) {
+	long long ans = 0;
+	while (num > 0) {
+		ans += tree[num];
+		num -= (num & -num);
+	}
+
+	return ans;
+}
+
+int main() {
+	int i;
+	int a;
+	long long answer = 0;
+	
+	cin >> n;
+	for (i = 1; i <= n; i++) {
+		cin >> a;
+		input[a] = i;
+	}
+	for (i = 1; i <= n; i++) {
+		cin >> a;
+		p[i] = input[a];
+	}
+
+	for (i = 1; i <= n; i++) {
+		update(p[i]);
+		answer += sum(n) - sum(p[i]);
+	}
+
+	cout << answer;
+}
