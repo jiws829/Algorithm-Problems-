bf8c5b0fa75a1bbcf55a45b0c475386310f18407
jiws829 <43372042+jiws829@users.noreply.github.com>
2020-05-19 13:17:37 +0900

    Add files via upload

---
+++ BOJ 12865_평범한 배낭(Knapsack).cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+int n, k;
+int p[102][100001];
+int weight[101], value[101];
+
+int max(int a, int b) {
+	if (a > b) return a;
+	else return b;
+}
+
+int main() {
+	int i, j;
+	
+	cin >> n >> k;
+	for (i = 1; i <= n; i++) cin >> weight[i] >> value[i];
+
+	for (i = 1; i <= n+1; i++) {
+		for (j = 1; j <= k; j++) {
+			if (weight[i-1] > j) p[i][j] = p[i - 1][j];
+			else p[i][j] = max(value[i-1]+p[i-1][j-weight[i-1]],p[i-1][j]);
+		}
+	}
+	cout << p[n+1][k];
+}
---
+++ BOJ 7579_앱(Knapsack).cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+
+int n, m;
+int byte[101], cost[101];
+int p[101][10001];
+
+int max(int a, int b) {
+	if (a > b) return a;
+	else return b;
+}
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+
+	int i, j;
+	int sum = 0;
+	int answer = -1;
+
+	cin >> n >> m;
+	for (i = 1; i <= n; i++) cin >> byte[i];
+	for (i = 1; i <= n; i++) {
+		cin >> cost[i];
+		sum += cost[i];
+	}
+
+	for (i = 1; i <= n; i++) {
+		for (j = 0; j <= sum; j++) {
+			if (j < cost[i]) p[i][j] = p[i - 1][j];
+			else p[i][j] = max(p[i - 1][j], byte[i] + p[i - 1][j - cost[i]]);
+
+			//p[i][j]는 i번째 프로세스를 봤을때 j의 cost를 사용해서 얻을수 있는 최대 메모리량
+		}
+	}
+
+	for (j = 0; j <= sum; j++) {
+		for (i = 1; i <= n; i++) {
+			if (p[i][j] >= m) {
+				answer = j;
+				break;
+			}
+		}
+		if (answer != -1) break;
+	}
+
+	cout << answer;
+}
