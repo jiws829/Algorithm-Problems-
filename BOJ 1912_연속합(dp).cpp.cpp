072a43d2c5b3398b8a3df757f5ca279cfabd73cb
jiws829 <43372042+jiws829@users.noreply.github.com>
2020-09-16 13:51:28 +0900

    Add files via upload

---
+++ BOJ 1912_연속합(dp).cpp.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+#include<string>
+#include<iostream>
+using namespace std;
+
+int p[1001][1001];
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+
+	string a, b;
+	int n, m;
+	int i, j;
+
+	cin >> a >> b;
+
+	n = a.size();
+	m = b.size();
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= m; j++) {
+			if (a[i - 1] == b[j - 1]) p[i][j] = p[i - 1][j - 1] + 1;
+			else {
+				if (p[i - 1][j] > p[i][j - 1]) p[i][j] = p[i - 1][j];
+				else p[i][j] = p[i][j - 1];
+			}
+		}
+	}
+
+	cout << p[n][m];
+}
